ff8b307818c3b650efbb5d358bca6d859e7ec93e
INDRIAN <indrian16@outlook.co.id>
2020-07-14 03:13:48 +0800

    membuat doc, di kodingan

--- js/logic.js
+++ js/logic.js
@@ -7,9 +7,13 @@ function changeMode(dark) {
 
     if (dark) {
         console.log("Mode Dark")
+
+        // Menganti Tema Dark
         darkTheme()
     } else {
         console.log("Mode Light")
+
+        // Menganti Tema Light
         lightTheme()
     }
 }
